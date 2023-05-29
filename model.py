5a57beb00c401eede423843ad50138a458c80bc6
RomanAYakunin <romario.yakunin@gmail.com>
2023-05-28 17:35:00 -0700

    SUB 5
    model size: 77.79294872283936 MB
    getting sequences: 100%|██████████| 121/121 [00:22<00:00,  5.33it/s]
    validating tflite model: 100%|██████████| 12182/12182 [09:19<00:00, 21.77it/s, mean accuracy = 0.638191991, mean pred time = 45.534056506 ms]
    accuracy: 0.6381919906836454
    pred time: 45.53405650631584 ms
    
    training acc is ~0.82

--- model.py
+++ model.py
@@ -120,8 +120,8 @@ class Model(nn.Module):  # TODO try copying hyperparams from transformer_branch
         self.feature_norms = nn.ModuleList([AxisLayerNorm(end - start, self.num_axes, 2)
                                             for start, end in self.norm_ranges])
 
-        self.dim = 780
-        self.num_heads = 78
+        self.dim = 1280
+        self.num_heads = 128
 
         self.input_net = nn.Sequential(
             nn.Linear(2 * self.num_points * self.num_axes, 2 * self.dim),  # TODO try turning off bias
@@ -135,6 +135,7 @@ class Model(nn.Module):  # TODO try copying hyperparams from transformer_branch
         self.sliding_attn1 = SlidingATTN(self.dim, num_heads=self.num_heads, window_size=5, dilation=1)
         self.sliding_attn2 = SlidingATTN(self.dim, num_heads=self.num_heads, window_size=5, dilation=3)
         self.sliding_attn3 = SlidingATTN(self.dim, num_heads=self.num_heads, window_size=5, dilation=3)
+        self.sliding_attn4 = SlidingATTN(self.dim, num_heads=self.num_heads, window_size=5, dilation=3)
         self.output_lin = nn.Linear(self.dim, 60)
 
     def forward(self, x):  # [N, L, num_points, num_axes]
@@ -149,7 +150,8 @@ class Model(nn.Module):  # TODO try copying hyperparams from transformer_branch
         sliding_attn1_out = self.sliding_attn1(input_net_out, mask)
         sliding_attn2_out = self.sliding_attn2(sliding_attn1_out, mask)
         sliding_attn3_out = self.sliding_attn3(sliding_attn2_out, mask)
-        out = self.output_lin(sliding_attn3_out)
+        sliding_attn4_out = self.sliding_attn4(sliding_attn3_out, mask)
+        out = self.output_lin(sliding_attn4_out)
         return out
 
 
