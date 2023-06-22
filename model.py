fixed pad masking & simplified end token logic

model.py
@@ -30,7 +30,8 @@ class Model(nn.Module):
         enc_out = self.enc(x, pad_mask)
         tokens = torch.full((x.shape[0], self.max_dec_len), fill_value=61, dtype=torch.long, device=x.device)
         tokens[:, 0] = 60
-        for i in range(self.max_dec_len - 1):  # TODO can shorten for loop
+        tokens[:, -1] = 59
+        for i in range(self.max_dec_len - 2):  # TODO can shorten for loop
             dec_out = self.dec(enc_out, tokens, pad_mask)
             tokens[:, i + 1] = torch.argmax(dec_out[:, i], dim=-1)
         return tokens[:, 1:]

training.py
@@ -53,11 +53,7 @@ def train(model, train_dataloader, epochs, optimizer, label_smooth=0.2, schedule
                             x, y = x.cuda(), y.cuda()
                             model_outputs = model.infer(x)
                             for output in model_outputs:  # TODO move this part to utils so it can be reused
-                                where_stop = torch.argwhere(output == 59).ravel()
-                                if torch.numel(where_stop) == 0:
-                                    outputs.append(output)
-                                else:
-                                    outputs.append(output[:where_stop[0]])
+                                outputs.append(output[:torch.argwhere(output == 59).ravel()[0]])
                             for label in y:
                                 labels.append(label[1:torch.argwhere(label == 59).ravel()[0]])
                 model.train()

validate_torch.py
@@ -12,7 +12,7 @@ import time
 from dataset import get_seqs
 from model import Model
 import torch
-from utils import proc_model_output, label_to_phrase
+from utils import label_to_phrase
 
 
 model_path = 'saved_models/test_model.pt'
@@ -24,6 +24,7 @@ model.load_state_dict(torch.load(model_path))
 model.eval()
 
 _, val_seq_ids = train_val_split()
+val_seq_ids = val_seq_ids[:100]
 seqs = get_seqs(val_seq_ids)
 labels = phrases_to_labels(get_phrases(val_seq_ids))
 
@@ -34,12 +35,10 @@ for i, (seq, label) in enumerate(pbar := tqdm(list(zip(seqs, labels)), file=sys.
     seq = seq.astype(np.float32)
     time_start = time.time()
     with torch.no_grad():
-        # seq = np.concatenate([seq, np.zeros((100, seq.shape[1], seq.shape[2]), dtype=np.float32)])  # TODO remove
-        output = model(torch.from_numpy(seq).unsqueeze(0)).squeeze(0).detach()
-        # output = output[:-100]  # TODO remove
+        # seq = np.concatenate([seq, np.zeros((300, seq.shape[1], seq.shape[2]), dtype=np.float32)])  # TODO remove
+        output = model.infer(torch.from_numpy(seq).unsqueeze(0)).squeeze(0)
+        output = output[:torch.argwhere(output == 59).ravel()[0]]
     time_sum += time.time() - time_start
-    output = torch.argmax(output, dim=-1)
-    output = proc_model_output(output)
     # print('\nphrase:', label_to_phrase(label.astype(np.int32)))
     # print('output:', label_to_phrase(output.cpu().numpy().astype(np.int32)))
     dist_sum += editdistance.eval(output.tolist(), label.tolist())