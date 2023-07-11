d2c33ae9217ca1b11dc678448ff13f8308284d52
RomanAYakunin <romario.yakunin@gmail.com>
2023-07-10 20:55:33 -0700

    SUB 26

--- train_on_all.py
+++ train_on_all.py
@@ -5,15 +5,15 @@ from torchinfo import summary
 from dataset import FeatureGenerator, get_dataloader, NPZDataset
 from utils import get_seq_ids
 
-train_seq_ids = get_seq_ids()
-NPZDataset.create(train_seq_ids, 'proc_data/all.npz')
+# train_seq_ids = get_seq_ids()
+# NPZDataset.create(train_seq_ids, 'proc_data/all.npz')
 
 batch_size = 64
 model = Model().cuda()
 FG = FeatureGenerator()
 summary(model, input_size=[(2, 170, FG.num_points, FG.num_axes), (2, 20)], dtypes=[torch.float32, torch.long])
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.01)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.1)
 
 train_dataloader = get_dataloader(save_path='proc_data/all.npz', batch_size=batch_size, shuffle=True)
 
